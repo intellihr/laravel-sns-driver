1e3957630cdf5e3ca31d8dad1a3492659826d4f5
Andrew Smith <espadav8@gmail.com>
2017-12-07 15:28:54 +1000

    Allow SNSClient to use configured AWS ENV variables if they've been configured

--- src/SnsBroadcastServiceProvider.php
+++ src/SnsBroadcastServiceProvider.php
@@ -16,23 +16,33 @@ class SnsBroadcastServiceProvider extends ServiceProvider
             ->extend(
                 'sns',
                 function ($app, $config) {
-                    return new SnsBroadcaster(
-                        SnsClient::factory(
-                            [
-                                'credentials' => [
-                                    'key' => $config['aws_key'],
-                                    'secret' => $config['aws_secret'],
-                                ],
-                                'version' => 'latest',
-                                'region' => $config['aws_region']
-                            ]
-                        )
-                    );
+                    $parameters = [
+                        'version' => 'latest',
+                        'region' => $config['aws_region'],
+                    ];
+
+                    if ($this->hasAWSEnvConfigured() === false) {
+                        $parameters['credentials'] = [
+                            'key' => $config['aws_key'],
+                            'secret' => $config['aws_secret'],
+                        ];
+                    }
+
+                    return new SnsBroadcaster(SnsClient::factory($parameters));
                 }
             );
     }
 
     public function register()
     {
+    }
+
+    private function hasAWSEnvConfigured()
+    {
+        if (getenv('AWS_ACCESS_KEY_ID') !== false && getenv('AWS_SECRET_ACCESS_KEY') !== false) {
+            return true;
+        }
+
+        return getenv('AWS_SESSION_TOKEN') !== false;
     }
 }
