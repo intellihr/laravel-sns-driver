9b0b63b97101b4b2e028db7b2247ad4d6beafc17
Andrew Smith <espadav8@gmail.com>
2017-12-12 16:55:27 +1000

    Make sure that all payloads have the tenant

--- src/SnsBroadcaster.php
+++ src/SnsBroadcaster.php
@@ -4,19 +4,35 @@ namespace Ringtrail\LaravelSns;
 
 use Aws\Sns\SnsClient;
 use Illuminate\Contracts\Broadcasting\Broadcaster;
+use Illuminate\Contracts\Config\Repository;
 
 class SnsBroadcaster implements Broadcaster
 {
+    /**
+     * @var SnsClient
+     */
     protected $client;
 
-    public function __construct(SnsClient $client)
-    {
+    /**
+     * @var Repository
+     */
+    protected $config;
+
+    public function __construct(
+        SnsClient $client,
+        Repository $config
+    ) {
         $this->client = $client;
+        $this->config = $config;
     }
 
     public function broadcast(array $channels, $event, array $payload = [])
     {
-        $payload = ['event' => $event, 'data' => $payload];
+        $payload = [
+            'event' => $event,
+            'data' => $payload,
+            'tenant' => $this->config->get('lapis.tenant', ''),
+        ];
 
         foreach ($channels as $channel) {
             $this
